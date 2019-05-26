9c01b5b355554481f52377e270f632c7ae8b66e4
Daniel Iglesias <diglesias78@gmail.com>
2019-05-26 20:09:14 +0100

    compareFiles added for testing
    to lmx_except.h

--- src/LMX/lmx_except.h
+++ src/LMX/lmx_except.h
@@ -45,6 +45,8 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <fstream>
+#include <iterator>
 
 using std::endl; using std::cout; using std::cerr;
 using std::ends; using std::cin;
@@ -305,6 +307,25 @@ namespace lmx {
   
 } /* end of namespace lmx.                                                */
 
+ /** Useful function for comparing test outputs. */
+bool compareFiles(const std::string& p1, const std::string& p2) {
+	std::ifstream f1(p1, std::ifstream::binary | std::ifstream::ate);
+	std::ifstream f2(p2, std::ifstream::binary | std::ifstream::ate);
 
+	if (f1.fail() || f2.fail()) {
+		return false; //file problem
+	}
+
+	if (f1.tellg() != f2.tellg()) {
+		return false; //size mismatch
+	}
+
+	//seek back to beginning and use std::equal to compare contents
+	f1.seekg(0, std::ifstream::beg);
+	f2.seekg(0, std::ifstream::beg);
+	return std::equal(std::istreambuf_iterator<char>(f1.rdbuf()),
+		std::istreambuf_iterator<char>(),
+		std::istreambuf_iterator<char>(f2.rdbuf()));
+}
 
 #endif /* LMX_EXCEPT_H__ */
